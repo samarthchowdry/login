92f6ea8548661261b49d97d85590de51d52f19d5
samarthchowdry <samarthchowdry1@gmail.com>
2025-10-21 22:45:57 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   username: string = '';
   password: string = '';
+  isLoggedIn: boolean = false;
 
   onLogin() {
-    if (this.username === 'admin' && this.password === '1234') {
+    if (this.username === 'sam' && this.password === '2345') {
+      this.isLoggedIn = true;
       alert('Login successful!');
     } else {
       alert('Invalid credentials');
     }
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.username = '';
+    this.password = '';
   }
 }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { HomeComponent } from './home/home.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    component: HomeComponent
+  },
+  {
+    path: 'items',
+    component: ItemsComponent
+  },
+  {
+    path: 'student-list',
+    component: StudentListComponent
+  },
+  {
+    path: 'edit-student/:id',
+    component: EditStudentComponent
   }
 ];
